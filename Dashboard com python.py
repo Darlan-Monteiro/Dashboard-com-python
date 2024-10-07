6d26c58aa45cd03d5c44030bf581d7b5b7609e82
Darlan Monteiro <darlanmonteirott@gmail.com>
2024-10-06 22:12:13 -0300

    Add files via upload

---
+++ Dashboard com python.py
@@ -0,0 +1,87 @@
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+import dash_auth
+
+usuarios = {'darlan': '12345678'}
+app = Dash(__name__)
+auth= dash_auth.BasicAuth(app, usuarios)
+
+df = pd.read_excel(r"C:\Users\SAMSUNG\OneDrive\Área de Trabalho\Documentos\Portfólio PYTHON\Dashboards com python\Vendas.xlsx")
+
+fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+fig2 = px.scatter(df, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
+
+lista_marcas = list(df["Marca"].unique())
+lista_marcas.append("Todas")
+
+lista_paises = list(df["País"].unique())
+lista_paises.append("Todos")
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Dashboard de Vendas em Python'),
+
+    html.Div(children='''
+        Desenvolvido por: Darlan Monteiro
+    '''),
+    
+    html.H3(children="Vendas de cada Produto por Loja", id="subtitulo"),
+    
+    dcc.RadioItems(options=lista_marcas, value="Todas", id='selecao_marcas'),
+    html.Div(children=[
+        dcc.Dropdown(options=lista_paises, value="Todos", id='selecao_pais'),
+    ], style={"width": "20%", "margin": "auto"}),
+    
+    dcc.Graph(id='vendas_por_loja',figure=fig),
+    dcc.Graph(id='distribuicao_vendas', figure=fig2),   
+], style={"text-align": "center"})
+
+
+
+@app.callback(
+    Output('selecao_pais', 'options'),
+    Input('selecao_marcas', 'value'),
+)
+def opcoes_pais(marca):
+    # criar uma lógica que diga qual a lista de paises que ele vai pegar
+    if marca == "Todas":
+        nova_lista_paises = list(df["País"].unique())
+        nova_lista_paises.append("Todos")
+    else:
+        df_filtrada = df.loc[df['Marca']==marca, :]
+        nova_lista_paises = list(df_filtrada["País"].unique())
+        nova_lista_paises.append("Todos")
+    return nova_lista_paises
+
+@app.callback(
+    Output('subtitulo','children'),
+    Output('vendas_por_loja','figure'),
+    Output('distribuicao_vendas','figure'),
+    Input('selecao_marcas', 'value'),
+    Input('selecao_pais', 'value'),
+    )
+
+def selecionar_marca(marca, pais):
+    if marca == "Todas" and pais == "Todos":
+        texto = "Vendas de cada Produto por Loja"
+        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig2 = px.scatter(df, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
+    else:
+        df_filtrada = df
+        if marca != "Todas":
+            #filtrar de acordo com a marca
+            df_filtrada = df_filtrada.loc[df_filtrada['Marca']==marca, :]
+        if pais != "Todos":
+            # filtrar de acordo com o pais
+            df_filtrada = df_filtrada.loc[df_filtrada["País"]==pais, :]
+        
+        texto = f"Vendas de cada Produto por Loja da Marca {marca} e do País {pais}"
+        fig = px.bar(df_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig2 = px.scatter(df_filtrada, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
+    return texto, fig, fig2
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
